9b50be85312688f7b93db5636d352f32c5292dda
Alaric-Jeff <jefsohandsome1@gmail.com>
2025-05-31 11:28:51 +0800

    added controller for deletion of acc

---
+++ Backend/src/controllers/accountController/DeleteAccountController.ts
@@ -0,0 +1,36 @@
+import DeleteAccount from "../../services/AccountLogics/DeleteAccount.js";
+import logger from "../../utils/logger.js";
+
+const DeleteAccountController = async (req: any, res: any) => {
+
+    const {userId } = req.params;
+
+    if(!userId){
+        logger.error("User ID is missing");
+
+        return res.status(400).json({
+            success: false,
+            error: "User ID is required"
+        });
+    }   
+
+    try{
+        await DeleteAccount(parseInt(userId));
+        logger.info("User deleted successfully:", userId);
+
+        return res.status(200).json({
+            success: true,
+            message: "User deleted successfully"
+        });
+    }catch(err: any){
+        logger.error("Error in DeleteAccountController:", err.message);
+        const statusCode = err.message === "User not found" ? 404 : 500;
+        const errMessage = err.message === "User not found" ? "User not found" : "Internal server error";
+        return res.status(statusCode).json({
+            success: false,
+            error: errMessage
+        });
+    }
+}
+
+export default DeleteAccountController; 
