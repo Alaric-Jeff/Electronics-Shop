feeb21d0ab41115800b8a8863cacaea56d6f0abe
Alaric-Jeff <jefsohandsome1@gmail.com>
2025-05-31 11:26:02 +0800

    added deletion of acc

---
+++ Backend/src/services/AccountLogics/DeleteAccount.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from "@prisma/client";
+import logger from "../../utils/logger.js";
+
+const prisma = new PrismaClient();
+
+async function DeleteAccount(id: number) {
+    try{
+        const user = await prisma.user.findUnique({
+            where: { id }
+        });
+
+        if (!user) {
+            logger.error("User not found:", id);
+            throw new Error("User not found");
+        }
+
+        await prisma.user.delete({
+            where: { id }
+        });
+
+        logger.info("User deleted successfully:", id);
+    }catch(err: any){
+        logger.error("Error deleting account:", err.message);
+        throw new Error("Failed to delete account");
+
+    }
+}
+
+export default DeleteAccount;
+
+
+   
