b04be7935427c1df6a45f3716ed5474c746bcaac
Alaric-Jeff <jefsohandsome1@gmail.com>
2025-06-15 03:18:36 +0800

    err passing fixed for stack tracing

--- Backend/src/services/productServices/getProducts.ts
+++ Backend/src/services/productServices/getProducts.ts
@@ -7,14 +7,9 @@ async function getProducts() {
     try {
 
         return await prisma.product.findMany();
-        
+
     } catch (err: unknown) {
-        if (err instanceof Error) {
-            logger.error("Error in getProducts:", err.message);
-        } else {
-            logger.error("Unknown error in getProducts:", String(err));
-        }
-        return new Error("Error in getting products service");
+        throw err;
     }
 }
 
