26b9ad962cc7cb4e9016d487d71c0254352da9ff
Alaric-Jeff <jefsohandsome1@gmail.com>
2025-07-15 17:19:40 +0800

    added stripe create customer

--- Backend/src/services/StripeService/customerServices/createCustomerService.ts
+++ Backend/src/services/StripeService/customerServices/createCustomerService.ts
@@ -1,22 +1,22 @@
-import stripe from "../../config/stripe.js";
-import logger from "../../utils/logger.js";
+import stripe from "../../../config/stripe.js";
+import logger from "../../../utils/logger.js";
 
 interface CustomerSchema{
     customerName: string,
     payment_method: string,
-    payment_method_type: string,
     email: string
     metadata?: {
         userId?: string,
         address?: string,
-        contactNumber?: number
+        contactNumber?: number,
+        
     };
 }
 
-async function createCustomerService({customerName, payment_method, email, payment_method_type, metadata = {}}: CustomerSchema){
+async function createCustomerService({customerName, payment_method, email, metadata = {}}: CustomerSchema){
 
-    if(!customerName || !payment_method || !email ||!payment_method_type || !metadata.userId || ! metadata.address || !metadata.contactNumber){
-        logger.debug(`recieve fields: customer name: ${customerName}, payment method id: ${payment_method}, email: ${email},payment type: ${payment_method_type}, user id: ${metadata.userId}, 
+    if(!customerName || !payment_method || !email || !metadata.userId || ! metadata.address || !metadata.contactNumber){
+        logger.debug(`recieve fields: customer name: ${customerName}, payment method id: ${payment_method}, email: ${email}, user id: ${metadata.userId}, 
             adress: ${metadata.address}, contact number: ${metadata.contactNumber}
             `)
             throw new Error("Required fields missing in the create customer service")
